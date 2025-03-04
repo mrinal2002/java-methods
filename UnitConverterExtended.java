e96718802cd07e450426bc0bb305c460f5649ffe
Mrinal Patra <mrinal2002patra@gmail.com>
2025-03-04 13:35:58 +0530

    [Mrinal] desc: unit convert basic programs extended

---
+++ UnitConverterExtended.java
@@ -0,0 +1,39 @@
+public class UnitConverterExtended {
+    // Convert yards to feet
+    public static double convertYardsToFeet(double yards) {
+        double yards2feet = 3;
+        return yards * yards2feet;
+    }
+
+    // Convert feet to yards
+    public static double convertFeetToYards(double feet) {
+        double feet2yards = 0.333333;
+        return feet * feet2yards;
+    }
+
+    // Convert meters to inches
+    public static double convertMetersToInches(double meters) {
+        double meters2inches = 39.3701;
+        return meters * meters2inches;
+    }
+
+    // Convert inches to meters
+    public static double convertInchesToMeters(double inches) {
+        double inches2meters = 0.0254;
+        return inches * inches2meters;
+    }
+
+    // Convert inches to centimeters
+    public static double convertInchesToCm(double inches) {
+        double inches2cm = 2.54;
+        return inches * inches2cm;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("10 yards to feet: " + convertYardsToFeet(10));
+        System.out.println("10 feet to yards: " + convertFeetToYards(10));
+        System.out.println("10 meters to inches: " + convertMetersToInches(10));
+        System.out.println("10 inches to meters: " + convertInchesToMeters(10));
+        System.out.println("10 inches to cm: " + convertInchesToCm(10));
+    }
+}
