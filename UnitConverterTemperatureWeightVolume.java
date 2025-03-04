fafe6a60f0c67f1143c29ab7d7844f872f15f22b
Mrinal Patra <mrinal2002patra@gmail.com>
2025-03-04 13:37:42 +0530

    [Mrinal] desc : unit conversation

---
+++ UnitConverterTemperatureWeightVolume.java
@@ -0,0 +1,44 @@
+public class UnitConverterTemperatureWeightVolume {
+    // Convert Fahrenheit to Celsius
+    public static double convertFahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    // Convert Celsius to Fahrenheit
+    public static double convertCelsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    // Convert pounds to kilograms
+    public static double convertPoundsToKilograms(double pounds) {
+        double pounds2kilograms = 0.453592;
+        return pounds * pounds2kilograms;
+    }
+
+    // Convert kilograms to pounds
+    public static double convertKilogramsToPounds(double kilograms) {
+        double kilograms2pounds = 2.20462;
+        return kilograms * kilograms2pounds;
+    }
+
+    // Convert gallons to liters
+    public static double convertGallonsToLiters(double gallons) {
+        double gallons2liters = 3.78541;
+        return gallons * gallons2liters;
+    }
+
+    // Convert liters to gallons
+    public static double convertLitersToGallons(double liters) {
+        double liters2gallons = 0.264172;
+        return liters * liters2gallons;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("100°F to Celsius: " + convertFahrenheitToCelsius(100));
+        System.out.println("37°C to Fahrenheit: " + convertCelsiusToFahrenheit(37));
+        System.out.println("150 pounds to kg: " + convertPoundsToKilograms(150));
+        System.out.println("70 kg to pounds: " + convertKilogramsToPounds(70));
+        System.out.println("10 gallons to liters: " + convertGallonsToLiters(10));
+        System.out.println("30 liters to gallons: " + convertLitersToGallons(30));
+    }
+}
